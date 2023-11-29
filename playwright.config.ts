81de1aa2e98a6cb61dc3a8788d1e11da541f364d
gabiliz <gabrielalizmoreira@gmail.com>
2023-11-29 19:23:14 -0300

    remove auth setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,6 @@ export default defineConfig({
   },
   // Configure projects for major browsers.
   projects: [
-    { name: 'setup', testMatch: /.*\.setup\.ts/ },
     {
       name: 'chromium',
       use: { 
