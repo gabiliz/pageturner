3d14142406dcd66f937bb03daf6e9c5ece24272f
gabiliz <gabrielalizmoreira@gmail.com>
2023-11-30 23:54:19 -0300

    fix test

--- src/tests/e2e/profile.spec.ts
+++ src/tests/e2e/profile.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 
-test('home page', async ({ page }) => {
+test('profile page', async ({ page }) => {
   await page.goto('https://pageturner.vercel.app/');
 
   await expect(page).toHaveTitle(/Pageturner/)
